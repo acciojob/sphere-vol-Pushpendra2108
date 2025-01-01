77c87cd65eb80a64b400da35158e7605a8bfcf8c
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2025-01-01 09:12:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	 event.preventDefault(); // Prevent form submission
+
+            const radius = parseFloat(document.getElementById('radius').value);
+
+            if (radius > 0) {
+                const volume = (4 / 3) * Math.PI * radius ** 3; // Simplified cube calculation
+                document.getElementById('volume').value = volume.toFixed(2);
+            } else {
+                alert('Enter a positive number for the radius.');
+            }
   
 } 
 
